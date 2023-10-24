26a430eaf51ec3d3c04ce9e037335009bb434645
OlaChenczke <147501092+OlaChenczke@users.noreply.github.com>
2023-10-24 13:38:14 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,26 +1,24 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+class Bank:
+
+    clients={}
+
+class Client:
+
+    def __init__(self, client_name, balance):
+        self.client_name = client_name
+        self.balance = balance
+
+    def cash_input(self, add_money):
+        self.add_money = add_money
+        add_money = input("How much money would you like to input?: ")
+        self.balance += add_money
+        print(f"Your balance is: {self.balance}")
+
+    def cash_withdrawal(self, subtract_money):
+        self.subtract_money = subtract_money
+        subtract_money = input("How much money would you like to withdrawal?: ")
+        self.balance -= subtract_money
+        print(f"Your balance is: {self.balance}")
+
+client1 = Client("Jan Kowalski", 1200)
+client2 = Client("Mariusz Pudzianowski", 1600)
